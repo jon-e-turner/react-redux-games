e4e97ba54fa27ce898175dc2b582d7e63512310c
Jon Turner <jon.e.turner@outlook.com>
2026-04-28 21:35:33 -0700

    feat: add tests for top nav

---
+++ __tests__/components/top-navigation.test.tsx
@@ -0,0 +1,20 @@
+import { describe, it, expect } from 'vitest';
+import { renderWithProviders } from '../test-utils';
+import TopNavigation from '~/components/top-navigation';
+import { BrowserRouter } from 'react-router';
+// import { defaultState } from '~/utils';
+// import configureAppStore from '~/store';
+
+describe('TopNavigation', async () => {
+  it('renders a back button', async () => {
+    const screen = await renderWithProviders(
+      <BrowserRouter>
+        <TopNavigation>
+          <div></div>
+        </TopNavigation>
+      </BrowserRouter>,
+    );
+
+    expect(screen.getByLabelText('navigate back')).toBeInTheDocument();
+  });
+});
